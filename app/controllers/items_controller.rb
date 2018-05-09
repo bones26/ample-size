b8d30788779a221320fdcc95a53262101ed44a7b
Jen Hilton <jen@jen-hiltons-macbook-pro-2.local>
2018-05-09 12:37:20 +1000

    changed product_image to just image in params in item_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:product_image, :user, :name, :description, :price, :category, :size, :image)
+      params.require(:item).permit(:image, :user, :name, :description, :price, :category, :size, :image)
     end
 end
